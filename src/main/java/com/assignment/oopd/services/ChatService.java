08643344d93e83315465f9ad21897c6e1810bc15
I524930 <aditi.sahai@sap.com>
2021-05-19 18:12:28 +0530

    Room_capacity issue fixed

--- src/main/java/com/assignment/oopd/services/ChatService.java
+++ src/main/java/com/assignment/oopd/services/ChatService.java
@@ -45,4 +45,43 @@ public class ChatService {
     public void setCapacity(Integer capacity) {
         this.capacity = capacity;
     }
+
+    public void leaveRoom(String roomId) throws NullPointerException {
+        try {
+            Integer[] b = roomCapacity.get(roomId);
+            if (b[0] > 0) {
+                b[0] = b[0] - 1;
+                if (b[0] == 0) {
+                    roomCapacity.remove(roomId);
+                    roomIds.remove(roomId);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("NullPointerException Occured");
+        }
+    }
+
+    public void addRoom(String roomId, Integer capacity) {
+        Integer[] room_cap = new Integer[] { 0, capacity };
+        roomCapacity.putIfAbsent(roomId, room_cap);
+    }
+
+    public void joinRoom(String roomId) {
+
+        Integer[] b = roomCapacity.get(roomId);
+        if (b[1] == b[0]) {
+
+        } else if (b[1] > b[0]) {
+            b[0]++;
+        }
+    }
+
+    public boolean checkCapacity(String roomId) {
+        Integer[] b = roomCapacity.get(roomId);
+        if (b[1] != b[0]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
